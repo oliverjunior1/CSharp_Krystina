f47b771d6e36052678ba474004e8977130d164a8
oliverjunior1 <oliver-junior@hotmail.com>
2024-10-03 19:36:02 -0300

    Create Program.cs

---
+++ Huw Collingbourne/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Jesus is the light of the World!");
+            //Console.ReadLine();
+            //----------------------------Change-the-color-on-console--------------------------------
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Jesus is the light of the World!");
+            Console.Read();
+        }
+    }
+}
